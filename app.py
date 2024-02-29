526cb16b56457649675ce4cd17e1bf23ed45617f
yahayakenny <y.hkehinde@yahoo.com>
2024-02-29 08:27:58 +0000

    testing

--- app.py
+++ app.py
@@ -98,15 +98,15 @@ def transcribe_audio(file_bytes, file_type, content_type):
 @cross_origin(origins=FRONT_END_URLS)
 def upload_audio():
 
-    if "audio" not in request.files:
-        return jsonify({"error": "No file part"}), 400
+    # if "audio" not in request.files:
+    #     return jsonify({"error": "No file part"}), 400
 
-    audio_file = request.files["audio"]
+    # audio_file = request.files["audio"]
 
-    if audio_file.filename == "":
-        return jsonify({"error": "No selected file"}), 400
+    # if audio_file.filename == "":
+    #     return jsonify({"error": "No selected file"}), 400
 
-    print("audio file>>>>", audio_file)
+    # print("audio file>>>>", audio_file)
 
     # if audio_file:
     #     filename = secure_filename(audio_file.filename)
